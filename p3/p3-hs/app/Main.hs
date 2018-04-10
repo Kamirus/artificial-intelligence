365eccdda700228181ac67dfdc22acc7377ce90f
Kamirus <listopadkamil@gmail.com>
2018-04-10 13:54:57 +0200

    main for p3

--- p3/p3-hs/app/Main.hs
+++ p3/p3-hs/app/Main.hs
@@ -1,8 +1,17 @@
 module Main
 where
 
-import           Lib
+import           Nonogram
+import           Nonogram.Inference
+import           Nonogram.Solver
+import           Nonogram.Util
 
 main :: IO ()
-main = someFunc
+main = do
+  (rows, cols) <- readInput
+  let nonogram = mkNonogram rows cols
+  printBoard $ board $ solve nonogram
+  -- case inferMax nonogram of
+  --   Left msg -> print msg
+  --   Right n -> printBoard $ board n
 
