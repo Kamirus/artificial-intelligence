ea7f57483889d67a3ec7bad5f6136abae5da6c03
Kamirus <listopadkamil@gmail.com>
2018-04-14 09:07:58 +0200

    shorter main, used >>>

--- p3/p3-hs/app/Main.hs
+++ p3/p3-hs/app/Main.hs
@@ -1,16 +1,11 @@
 module Main
 where
 
+import           Control.Category               ( (>>>) )
 import           Nonogram
 import           Nonogram.Inference
 import           Nonogram.Solver
 import           Nonogram.Util
 
 main :: IO ()
-main = do
-  (rows, cols) <- readInput
-  let nonogram = mkNonogram rows cols
-  printBoard $ board $ solve nonogram
-  -- case inferMax nonogram of
-  --   Left msg -> print msg
-  --   Right n -> printBoard $ board n
+main = uncurry mkNonogram <$> readInput >>= (solve >>> board >>> printBoard)
