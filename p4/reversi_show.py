5a0971fcc15c99dc815d35519d837d57092acc30
Kamirus <listopadkamil@gmail.com>
2018-04-22 19:54:44 +0200

    styling reversi

--- p4/reversi_show.py
+++ p4/reversi_show.py
@@ -2,11 +2,12 @@ import random
 import sys
 from collections import defaultdict as dd
 from turtle import *
+from typing import List, Tuple, Optional
 
 #####################################################
 # turtle graphic
 #####################################################
-tracer(0,1)
+tracer(0, 1)  # type: ignore
 
 BOK = 30
 SX = -100
@@ -15,51 +16,50 @@ M = 8
 
 
 def kwadrat(x, y, kolor):
-  fillcolor(kolor)
-  pu()
-  goto(SX + x * BOK, SY + y * BOK)
-  pd()
-  begin_fill()
-  for i in range(4):
-    fd(BOK)
-    rt(90)
-  end_fill() 
+    fillcolor(kolor)
+    pu()
+    goto(SX + x * BOK, SY + y * BOK)
+    pd()
+    begin_fill()
+    for i in range(4):
+        fd(BOK)
+        rt(90)
+    end_fill()
+
 
 def kolko(x, y, kolor):
-  fillcolor(kolor)
+    fillcolor(kolor)
 
-  pu()
-  goto(SX + x * BOK + BOK/2, SY + y * BOK - BOK)
-  pd()
-  begin_fill()
-  circle(BOK/2)
-  end_fill() 
+    pu()
+    goto(SX + x * BOK + BOK/2, SY + y * BOK - BOK)
+    pd()
+    begin_fill()
+    circle(BOK/2)
+    end_fill()
 
 #####################################################
 
+
 def initial_board():
-    B = [ [None] * M for i in range(M)]
+    B = [[None] * M for i in range(M)]
     B[3][3] = 1
     B[4][4] = 1
     B[3][4] = 0
     B[4][3] = 0
     return B
 
-    
+
 class Board:
-    dirs  = [ (0,1), (1,0), (-1,0), (0,-1), (1,1), (-1,-1), (1,-1), (-1,1) ]
-    
-    
+    dirs = [(0, 1), (1, 0), (-1, 0), (0, -1),
+            (1, 1), (-1, -1), (1, -1), (-1, 1)]
+
     def __init__(self):
         self.board = initial_board()
-        self.fields = set()
+        self.fields = {(j, i) for i in range(M) for j in range(M)
+                       if self.board[i][j] == None}
         self.move_list = []
         self.history = []
-        for i in range(M):
-            for j in range(M):
-                if self.board[i][j] == None:   
-                    self.fields.add( (j,i) )
-                                                
+
     def draw(self):
         for i in range(M):
             res = []
@@ -71,94 +71,93 @@ class Board:
                     res.append('#')
                 else:
                     res.append('o')
-            print ''.join(res) 
-        print            
-        
-    
+            print(''.join(res))
+        print()
+
     def show(self):
         for i in range(M):
             for j in range(M):
                 kwadrat(j, i, 'green')
-                
+
         for i in range(M):
-            for j in range(M):                
+            for j in range(M):
                 if self.board[i][j] == 1:
                     kolko(j, i, 'black')
                 if self.board[i][j] == 0:
                     kolko(j, i, 'white')
-                                   
-    def moves(self, player):
+
+    def moves(self, player: int) -> List[Tuple[int, int]]:
         res = []
-        for (x,y) in self.fields:
-            if any( self.can_beat(x,y, direction, player) for direction in Board.dirs):
-                res.append( (x,y) )
-        if not res:
-            return [None]
-        return res               
-    
-    def can_beat(self, x,y, d, player):
-        dx,dy = d
+        for (x, y) in self.fields:
+            if any(self.can_beat(x, y, direction, player) for direction in Board.dirs):
+                res.append((x, y))
+        # if not res:
+        #     return [None]
+        return res
+
+    def can_beat(self, x: int, y: int, direction: Tuple[int, int], player: int) -> bool:
+        dx, dy = direction
         x += dx
         y += dy
         cnt = 0
-        while self.get(x,y) == 1-player:
+        while self.get(x, y) == 1 - player:
             x += dx
             y += dy
             cnt += 1
-        return cnt > 0 and self.get(x,y) == player
-    
-    def get(self, x,y):
-        if 0 <= x < M and 0 <=y < M:
+        return cnt > 0 and self.get(x, y) == player
+
+    def get(self, x: int, y: int) -> Optional[int]:
+        if 0 <= x < M and 0 <= y < M:
             return self.board[y][x]
         return None
-                        
-    def do_move(self, move, player):
-        self.history.append([x[:] for x in self.board])
+
+    def do_move(self, move: Optional[Tuple[int, int]], player: int) -> None:
+        self.history.append([row[:] for row in self.board])
         self.move_list.append(move)
-        
-        if move == None:
+
+        if move is None:
             return
-        x,y = move
-        x0,y0 = move
+        x, y = move
+        x0, y0 = move
         self.board[y][x] = player
-        self.fields -= set([move])
-        for dx,dy in self.dirs:
-            x,y = x0,y0
+        self.fields.remove(move)  # -= set([move])
+        for dx, dy in self.dirs:
+            x, y = x0, y0
             to_beat = []
             x += dx
             y += dy
-            while self.get(x,y) == 1-player:
-              to_beat.append( (x,y) )
-              x += dx
-              y += dy
-            if self.get(x,y) == player:              
-                for (nx,ny) in to_beat:
+            while self.get(x, y) == 1 - player:
+                to_beat.append((x, y))
+                x += dx
+                y += dy
+            if self.get(x, y) == player:
+                for (nx, ny) in to_beat:
                     self.board[ny][nx] = player
-                                                     
-    def result(self):
+
+    def result(self) -> int:
         res = 0
         for y in range(M):
             for x in range(M):
-                b = self.board[y][x]                
+                b = self.board[y][x]
                 if b == 0:
                     res -= 1
                 elif b == 1:
                     res += 1
         return res
-                
-    def terminal(self):
+
+    def terminal(self) -> bool:
         if not self.fields:
             return True
         if len(self.move_list) < 2:
             return False
-        return self.move_list[-1] == self.move_list[-2] == None 
+        return self.move_list[-1] == self.move_list[-2] == None
 
-    def random_move(self, player):
+    def random_move(self, player: int) -> Optional[Tuple[int, int]]:
         ms = self.moves(player)
         if ms:
             return random.choice(ms)
-        return [None]    
-    
+        return None
+
 
 player = 0
 B = Board()
@@ -169,14 +168,14 @@ while True:
     m = B.random_move(player)
     B.do_move(m, player)
     player = 1-player
-    raw_input()
+    input()  # raw_input()
     if B.terminal():
         break
-    
+
 B.draw()
 B.show()
-print 'Result', B.result()
-raw_input('Game over!')
-  
-       
-sys.exit(0)                 
+print('Result', B.result())
+input('Game over!')  # raw_input('Game over!')
+
+
+sys.exit(0)
